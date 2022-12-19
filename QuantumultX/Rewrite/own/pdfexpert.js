b3aea54ed91c97a93d39b592446965ea5b0f955b
elnfnoosm <wwfnle@gmail.com>
2022-12-19 09:31:31 +0800

    更新 pdfexpert.js

--- QuantumultX/Rewrite/own/pdfexpert.js
+++ QuantumultX/Rewrite/own/pdfexpert.js
@@ -1,6 +1,6 @@
-#!name=❏ Scanner
+#!name=https://raw.githubusercontent.com/langkhach270389/Surge-LK/main/scripts/langkhach/pdfexpert.js
 #!desc=Documents-PdfExpert-Scaner Pro
 [MITM]
 hostname = %APPEND% license.pdfexpert.com
 [Script]
-Documents-PdfExpert = type=http-response,pattern=^https:\/\/license\.pdfexpert\.com\/api\/.+\/subscription\/(refresh$|check$),requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/langkhach270389/Surge-LK/main/scripts/langkhach/pdfexpert.js,script-update-interval=-1
+Documents-PdfExpert = type=http-response,pattern=^https:\/\/license\.pdfexpert\.com\/api\/.+\/subscription\/(refresh$|check$),requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/documents.js,script-update-interval=-1
