dd4014559f65949d638c6e6117ee7f74ef0f03c5
elnfnoosm <wwfnle@gmail.com>
2022-12-19 09:41:45 +0800

    更新 pdfexpert.js

--- QuantumultX/Rewrite/own/pdfexpert.js
+++ QuantumultX/Rewrite/own/pdfexpert.js
@@ -17,7 +17,7 @@ TG频道群：https://t.me/py
 
 [rewrite_local]
 
-^https:\/\/license\.pdfexpert\.com\/api\/2\.0\/pdfexpert6\/subscription\/refresh url script-request-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/PDFExpert.js
+^https:\/\/license\.pdfexpert\.com\/api\/2\.0\/pdfexpert6\/subscription\/refresh$ url script-request-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/PDFExpert.js
 
 [mitm] 
 
