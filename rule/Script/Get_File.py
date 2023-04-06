111c7ebe281971455097279a0499e766c39ae4fa
elnfnoosm <wwfnle@gmail.com>
2023-04-06 19:28:16 +0800

    Update Get_File.py

--- rule/Script/Get_File.py
+++ rule/Script/Get_File.py
@@ -3,8 +3,8 @@ import io
 import shutil
 import time
 import requests
+print (os.getcwd())
 
-os.chdir (./rule/)
 
 RULE_URL = "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Loon/"
 REJECT_RULES = {
